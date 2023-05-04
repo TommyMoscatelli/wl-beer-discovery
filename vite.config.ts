33a860f94de83ad52c64beccf052a73f84ba84b3
Gianluca Cappai <gianluca.cappai@winelivery.com>
2023-05-04 17:44:42 +0200

    Change vase path in config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/public/gin-discovery/',
+  base: '/public/beer-discovery/',
   plugins: [react()],
 })

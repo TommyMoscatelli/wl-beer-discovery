f23dd224f7c1ab85c1aef7d72ce7d63933c98eee
Gianluca Cappai <gianluca.cappai@winelivery.com>
2023-05-05 16:53:58 +0200

    Refactor Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,14 +11,12 @@ function Home() {
   return (
     <div className="flex flex-col justify-between h-full bg-[url('/img/background/dark.jpg')] bg-cover bg-center">
       <Header />
-      <div>
-        {/* BODY */}
-        <img
-          className="w-2/3 max-w-md mx-auto"
-          src={`${import.meta.env.BASE_URL}img/discovery_logo.png`}
-          alt="The beer discovery"
-        />
-      </div>
+
+      <img
+        className="w-2/3 max-w-md mx-auto"
+        src={`${import.meta.env.BASE_URL}img/discovery_logo.png`}
+        alt="The beer discovery"
+      />
 
       <Footer version="light">
         <Button text={t('cta')} href="/discovery" />

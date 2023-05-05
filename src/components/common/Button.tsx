c8c662a86f37862fb6b45258955cce44c1426991
Gianluca Cappai <gianluca.cappai@winelivery.com>
2023-05-05 12:06:49 +0200

    Create Button

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import { Arrow } from '../icons';
+
+interface Props {
+  text: string;
+}
+
+export default function Button({ text }: Props) {
+  return (
+    <Link
+      className="py-3 relative grow bg-primary-dark text-white rounded-md font-medium text-base text-center tracking-tight"
+      to="/discovery"
+    >
+      {text}
+      <Arrow className="absolute right-4 top-1/2 -translate-y-1/2" />
+    </Link>
+  );
+}

5820b12b1c68a2455eaa6b9516b69b5c753a4025
Gianluca Cappai <gianluca.cappai@winelivery.com>
2023-05-05 16:43:30 +0200

    Update Step1

--- src/components/Discovery/Step1.tsx
+++ src/components/Discovery/Step1.tsx
@@ -1,58 +1,44 @@
-import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { findCategories } from './decisionTree';
-import { Chevron } from '../icons';
+import { events } from './decisionTree';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+import ChoiceChip from '../common/ChoiceChip';
 
 export default function Step1() {
   const { t } = useTranslation('discovery');
-  const categories = findCategories();
 
   return (
-    <div className="flex flex-col h-full bg-[url('/img/background/base.jpg')] bg-cover bg-center px-4 py-6">
-      <div className="flex items-center gap-2 mb-6">
-        <Link to="/" className="h-2 w-3 rounded-full bg-gray-400" />
-        <div className="h-2 w-8 rounded-full bg-blue-800" />
-        <div className="h-2 w-3 rounded-full bg-gray-400" />
-        <div className="h-2 w-3 rounded-full bg-gray-400" />
+    <div className="flex flex-col justify-between h-full bg-[url('/img/background/light.jpg')] bg-cover bg-center">
+      <Header />
+
+      <div className="grow px-10 py-12 flex flex-col">
+        <h1 className="text-3xl font-bold tracking-tight text-primary mb-10 text-center">
+          {t('step1.title')}
+        </h1>
+
+        <ul className="space-y-8 grow flex flex-col justify-center">
+          {events.map((event) => (
+            <ChoiceChip
+              to={event.slug}
+              key={event.slug}
+              category={event.slug}
+              imgUrl={event.image}
+              text={t(`event.${event.slug}`)}
+            />
+          ))}
+        </ul>
       </div>
-      <h1 className="text-3xl font-bold tracking-tighter text-gray-800 mb-8 text-center">
-        {t('step1.title')}
-      </h1>
-      <ul className="flex-1 space-y-4">
-        {categories.map((category) => (
-          <li key={category.slug}>
-            <Link
-              className="flex items-center gap-4 p-4 rounded-full bg-blue-50 border border-blue-200 text-gray-700"
-              to={`/discovery/${category.slug}`}
-            >
-              <img
-                src={`${import.meta.env.BASE_URL}${category.image}`}
-                alt={category.slug}
-                className="h-12 shrink-0"
-              />
-              <span className="flex-1 text-xl font-medium tracking-tight">
-                {t(`category.${category.slug}`)}
-              </span>
-              <Chevron className="w-6 shrink-0" />
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className="flex gap-4 border border-blue-200 p-4 rounded-md bg-blue-50">
-        <img
-          src={`${import.meta.env.BASE_URL}img/curiosita.png`}
-          alt="Curiosità"
-          className="h-14 shrink-0"
-        />
-        <div className="space-y-2">
-          <h2 className="text-lg font-bold tracking-tight text-gray-800">
-            {t('step1.hint.title')}
+
+      <Footer>
+        <div className="text-center">
+          <h2 className="text-lg font-bold tracking-tight text-primary-dark">
+            {t('step1.fact.title')}
           </h2>
-          <p className="text-base font-medium tracking-tight text-gray-600">
-            {t('step1.hint.description')}
+          <p className="text-base font-medium tracking-tight text-primary-dark">
+            {t('step1.fact.description')}
           </p>
         </div>
-      </div>
+      </Footer>
     </div>
   );
 }

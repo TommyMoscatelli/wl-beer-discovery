4d38fc8043cf1759f6c5f25d0ef00cd84f192809
Gianluca Cappai <gianluca.cappai@winelivery.com>
2023-05-08 12:21:07 +0200

    Add more explicit background image class

--- src/components/common/ScreenContainer.tsx
+++ src/components/common/ScreenContainer.tsx
@@ -5,9 +5,14 @@ interface Props extends PropsWithChildren {
 }
 
 export default function ScreenContainer({ variant, children }: Props) {
+  const backgroundClass =
+    variant === 'light'
+      ? "bg-[url('/img/background/light.jpg')]"
+      : "bg-[url('/img/background/dark.jpg')]";
+
   return (
     <div
-      className={`flex flex-col justify-between h-full bg-[url('/img/background/${variant}.jpg')] bg-cover bg-center`}
+      className={`flex flex-col justify-between h-full ${backgroundClass} bg-cover bg-center`}
     >
       {children}
     </div>

0006385ea139dc023ed0ef6adb572092f36c36fb
Gianluca Cappai <gianluca.cappai@winelivery.com>
2023-05-08 15:06:23 +0200

    Add titleColor props on ScreenBody

--- src/components/common/ScreenBody.tsx
+++ src/components/common/ScreenBody.tsx
@@ -2,12 +2,18 @@ import { PropsWithChildren } from 'react';
 
 interface Props extends PropsWithChildren {
   title: string;
+  titleColor?: 'default' | 'dark';
 }
 
-export default function ScreenBody({ title, children }: Props) {
+export default function ScreenBody({ title, titleColor, children }: Props) {
+  const titleColorClass =
+    titleColor === 'default' ? 'text-primary' : 'text-primary-dark';
+
   return (
     <div className="grow px-10 py-12 flex flex-col">
-      <h1 className="text-3xl font-bold tracking-tight text-primary mb-10 text-center">
+      <h1
+        className={`text-3xl font-bold tracking-tigh mb-10 text-center ${titleColorClass}`}
+      >
         {title}
       </h1>
 
@@ -15,3 +21,7 @@ export default function ScreenBody({ title, children }: Props) {
     </div>
   );
 }
+
+ScreenBody.defaultProps = {
+  titleColor: 'default',
+};

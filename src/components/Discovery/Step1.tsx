08e30d856f022eba191a3097e108aab73a822922
Gianluca Cappai <gianluca.cappai@winelivery.com>
2023-05-08 11:58:25 +0200

    Refactory Step1

--- src/components/Discovery/Step1.tsx
+++ src/components/Discovery/Step1.tsx
@@ -2,43 +2,28 @@ import { useTranslation } from 'react-i18next';
 import { events } from './decisionTree';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
-import ChoiceChip from '../common/ChoiceChip';
+import ScreenContainer from '../common/ScreenContainer';
+import ScreenBody from '../common/ScreenBody';
+import ChoiceList from '../common/ChoiceList';
+import Fact from '../common/Fact';
 
 export default function Step1() {
   const { t } = useTranslation('discovery');
 
   return (
-    <div className="flex flex-col justify-between h-full bg-[url('/img/background/light.jpg')] bg-cover bg-center">
+    <ScreenContainer>
       <Header />
 
-      <div className="grow px-10 py-12 flex flex-col">
-        <h1 className="text-3xl font-bold tracking-tight text-primary mb-10 text-center">
-          {t('step1.title')}
-        </h1>
-
-        <ul className="space-y-8 grow flex flex-col justify-center">
-          {events.map((event) => (
-            <ChoiceChip
-              to={event.slug}
-              key={event.slug}
-              category={event.slug}
-              imgUrl={event.image}
-              text={t(`event.${event.slug}`)}
-            />
-          ))}
-        </ul>
-      </div>
+      <ScreenBody title={t('step1.title')}>
+        <ChoiceList type="event" data={events} />
+      </ScreenBody>
 
       <Footer>
-        <div className="text-center">
-          <h2 className="text-lg font-bold tracking-tight text-primary-dark">
-            {t('step1.fact.title')}
-          </h2>
-          <p className="text-base font-medium tracking-tight text-primary-dark">
-            {t('step1.fact.description')}
-          </p>
-        </div>
+        <Fact
+          title={t('step1.fact.title')}
+          description={t('step1.fact.description')}
+        />
       </Footer>
-    </div>
+    </ScreenContainer>
   );
 }
